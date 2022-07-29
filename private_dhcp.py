26e7e5382db8c0cb1e766d31ff45dc99534433e6
Matt Perry <mjperry82@gmail.com>
2022-07-29 11:02:19 -0500

    Updated private_dhcp.py to generate output.csv

--- private_dhcp.py
+++ private_dhcp.py
@@ -20,9 +20,10 @@ thread_local = threading.local()
 # List to hold router names and Number of private leases
 lease_list = []
 
-# path to routers.csv
+# path to routers.csv and output.csv
 current_dir = Path.cwd()
 router_file = current_dir / 'routers.csv'
+output_file = current_dir / 'output.csv'
 
 # private dhcp range
 private_range = '172.16.0.0/12'
@@ -45,7 +46,7 @@ def insert_leases(router):
     ssh = tik_ssh.connect(str(ip), creds.username, creds.password)
     
     if ssh != None:
-        command = f"/ip dhcp-server lease print count-only where address in {private_range}"
+        command = f"/ip dhcp-server lease print count-only where status=bound and address in {private_range}"
     
     output = tik_ssh.command(ssh, command)
     
@@ -56,11 +57,20 @@ def poplulate_leases(routers):
     with concurrent.futures.ThreadPoolExecutor(max_workers=40) as executor:
         executor.map(insert_leases,routers)
 
+def output_csv(output_file):
+    with open(output_file, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        for line in lease_list:
+            if int(line[2]) > 0:
+                writer.writerow(line)
+
 def main():
     routers = load_routers(router_file)
         
     poplulate_leases(routers)
     
+    output_csv(output_file)
 
 if __name__ == "__main__":
     main()
